3b44da664246337374d657c5e84cce8575846b8b
vazhid <vazhid64@gmail.com>
2023-08-03 21:55:06 +0300

    L6

--- src/predict.py
+++ src/predict.py
@@ -1,9 +1,15 @@
+import argparse
+from catboost import CatBoostClassifier
 import pandas as pd
+import numpy as np
 import re
 from pymorphy2 import MorphAnalyzer
 
-filename = ""
-df = pd.read_csv(filename)
+parser = argparse.ArgumentParser(description='Filename')
+parser.add_argument('filename', type=str, help='Name of the data file')
+args = parser.parse_args()
+
+df = pd.read_csv(args.filename)
 
 m = MorphAnalyzer()
 regex = re.compile("[А-Яа-яA-z]+")
@@ -25,7 +31,7 @@ def clean_text(text):
     lemmas = lemmatize_text(tokens)
     return lemmas
 
-lemmas = list(map(clean_text, df['text']), total=len(df))
+lemmas = list(map(clean_text, df['text']))
 df['lemmas'] = lemmas
 df['lemmas'] = df.lemmas.apply(lambda x: " ".join(x))
 
@@ -35,4 +41,11 @@ df["f1/f7"] = df["f1"]/df["f7"]
 df["f4/f7"] = df["f4"]/df["f7"]
 df["f3*f6"] = df["f3"]*df["f6"]
 
-df.to_csv('data.csv', index=True)
+df = df[["f1/f2", "lemmas", "text", "f7/f8", "f6", "f3*f6", "f4/f7", "f1/f7", "f3"]]
+
+model = CatBoostClassifier()
+model.load_model('../models/model')
+
+pred = model.predict(df)
+
+np.savetxt("../results/results.txt", pred, newline="\n", fmt="%d")
